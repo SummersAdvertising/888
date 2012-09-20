5b07a017281081faae21a8871892cd2c6333e8ba
Hana Hsu <kobanae@gmail.com>
2012-09-20 18:04:39 +0800

    修正搜尋、套板前同步

--- 888/js/loadData.js
+++ 888/js/loadData.js
@@ -15,16 +15,7 @@
             createDB();
         }
         else {
-            //clear objectstores of article and subject, load data
-            var txn = Data.db.transaction(["articles"], "readwrite");
-            var store = txn.objectStore("articles");
-            store.clear();
-
-            txn = Data.db.transaction(["subjects"], "readwrite");
-            store = txn.objectStore("subjects");
-            store.clear();
-
-            loadData(evt);
+            loadArray("0");
         }
     }
     function dbVersionUpgrade(evt) {
@@ -39,14 +30,20 @@
 
         loadData(evt);
     }
-    //declare WinJS.Binding.List
+
+    //declare WinJS.Binding.List(for search)
     var list = new WinJS.Binding.List();
-    var subject = new Array();//groups in list
-    var article = new Array();//groupitems in list
+
+
+    //declare bindinglist(for article)
+    var articlelist = new WinJS.Binding.List();
+    var subjectArray = new Array();//groups in list
+    var articleArray = new Array();//groupitems in list
 
     function loadData(evt) {
         var foldername = "data";
         var filename = Data.language + ".xml";
+        subjectArray = new Array();
 
         Data.db = evt.target.result;
 
@@ -56,7 +53,6 @@
                 loadSettings.prohibitDtd = false;
                 loadSettings.resolveExternals = false;
 
-                //load XML
                 Windows.Data.Xml.Dom.XmlDocument.loadFromFileAsync(file, loadSettings).done(function (xmlDoc) {
                     //load subject nodes
                     var nodes = xmlDoc.getElementsByTagName('subject');
@@ -71,16 +67,7 @@
                         var subjectsStore = txn.objectStore("subjects");
 
                         subjectsStore.put(objarticle);
-                        subject.push(objarticle);
-
-                        txn.oncomplete = function () {
-                            if (counter >= nodes.length)
-                                showData("subjects");
-                            if (list._currentKey == 0)
-                                listAdd();
-
-                            counter++;
-                        };
+                        subjectArray.push(objarticle);
                     }
 
                     //load article nodes
@@ -91,26 +78,29 @@
                         for (var j = 0; j < nodes[i].childNodes.length; j++) {
                             if (nodes[i].childNodes[j].nodeType == 1) {
                                 if (nodes[i].childNodes[j].nodeName == "subjectid") {
-                                    switch (nodes[i].childNodes[j].innerText) {
-                                        case "1":
-                                            objAddProperty("group", subject[0]);
-                                            break;
-                                        case "2":
-                                            objAddProperty("group", subject[1]);
-                                            break;
-                                        case "3":
-                                            objAddProperty("group", subject[2]);
-                                            break;
-                                        case "4":
-                                            objAddProperty("group", subject[3]);
-                                            break;
-                                        case "5":
-                                            objAddProperty("group", subject[4]);
-                                            break;
-                                        case "6":
-                                            objAddProperty("group", subject[5]);
-                                            break;
-                                    }
+                                    var group = parseInt(nodes[i].childNodes[j].innerText) - 1;
+                                    objAddProperty("group", subjectArray[group]);
+
+                                    //switch (nodes[i].childNodes[j].innerText) {
+                                    //    case "1":
+                                    //        objAddProperty("group", subjectArray[0]);
+                                    //        break;
+                                    //    case "2":
+                                    //        objAddProperty("group", subjectArray[1]);
+                                    //        break;
+                                    //    case "3":
+                                    //        objAddProperty("group", subjectArray[2]);
+                                    //        break;
+                                    //    case "4":
+                                    //        objAddProperty("group", subjectArray[3]);
+                                    //        break;
+                                    //    case "5":
+                                    //        objAddProperty("group", subjectArray[4]);
+                                    //        break;
+                                    //    case "6":
+                                    //        objAddProperty("group", subjectArray[5]);
+                                    //        break;
+                                    //}
                                 }
                                 //save content without html tag
                                 if (nodes[i].childNodes[j].nodeName == "content") {
@@ -126,46 +116,78 @@
                         var txn = Data.db.transaction(["articles"], "readwrite");
                         var articlesStore = txn.objectStore("articles");
                         articlesStore.add(objarticle);
-                        article.push(objarticle);
+                        //article.push(objarticle);
 
+                        var isdone = false;
                         txn.oncomplete = function () {
-                            counter++;
-                            if (counter >= nodes.length) {
-                                showData("articles");
-                                if (Data.articleid)
-                                    showData("article");
-                                counter = 1;
+                            //update data in page
+                            if (!isdone) {
+                                loadArray("0");
+
+                                switch (WinJS.Navigation.location) {
+                                    case "/pages/article/article.html":
+                                        showData("article");
+                                        break;
+                                    case "/pages/favorite/favorite.html":
+                                        //showData("favorite");
+                                        break;
+                                }
                             }
+                            isdone = true;
                         };
                     }
                 });
             });
         });
     }
+    function loadArray(region) {
+        articleArray = new Array();
+
+        var length = list.length;
+        for (var i = 0; i < length; i++) {
+            list.pop();
+        }
+
+        var txn = Data.db.transaction(["articles"], "readonly");
+        var store = txn.objectStore("articles");
+        var request = store.openCursor();
+        request.onsuccess = function (e) {
+            var article = e.target.result;
+            if (article) {
+                if (article.value["region"] == region)
+                    articleArray.push(article.value);
+                list.push(article.value);
+                article.continue();
+            }
+            else
+                listAdd();
+        };
+    }
 
     function listAdd() {
-        article.forEach(function (item) {
-            list.push(item);
+        //remove old data
+        var length = articlelist.length;
+        for (var i = 0; i < length; i++) {
+            articlelist.pop();
+        }
+
+        //add new ones
+        articleArray.forEach(function (item) {
+            articlelist.push(item);
         });
-        groupedItems = list.createGrouped(
-                    function groupKeySelector(item) { return item.group.key; },
-                    function groupDataSelector(item) { return item.group; }
-                );
     }
 
     //data for listview
-    var myGroupedList = list.createGrouped(getGroupKey, getGroupData, compareGroups);
+    var myGroupedList = articlelist.createGrouped(getGroupKey, getGroupData, compareGroups);
 
     // Function used to sort the groups by first letter
     function compareGroups(left, right) {
         return left.charCodeAt(0) - right.charCodeAt(0);
     }
-
     // Function which returns the group key that an item belongs to
     function getGroupKey(dataItem) {
         return dataItem.subjectid;
     }
-
     // Function which returns the data for a group
     function getGroupData(dataItem) {
         return {
@@ -185,85 +207,75 @@
 
     function showData(show) {
         switch (show) {
-            //list all the articles
-            case "articles":
-                var txn = Data.db.transaction(["articles"], "readonly");
-                var cursorRequest = txn.objectStore("articles").openCursor();
-                var articles = "";
-                cursorRequest.onsuccess = function (e) {
-                    var cursor = e.target.result;
-                    if (cursor) {
-                        var str = "";
-                        for (var i in cursor.value) {
-                            if (i != "content")
-                                str += i.toString() + ": " + cursor.value[i] + " / ";
-                        }
-                        articles += "<p>" + str + "<a class='article' id='article" + cursor.value["id"] + "' href='/pages/article/article.html'>show</a></p>";
-
-                        cursor.continue();
-                    }
-                    $("#output").html(articles);
-                    $(".article").unbind();
-                    $(".article").bind("click", function () { Home.showArticle(this); return false; });
-                }
-                break;
-
-                //list a single article
             case "article":
-                var txn = Data.db.transaction(["articles"], "readonly");
+                var txn = Data.db.transaction(["articles"], "readwrite");
                 var store = txn.objectStore("articles");
                 var request = store.get(parseInt(Data.articleid));
                 var articles = "";
+
                 request.onsuccess = function (e) {
                     var article = e.target.result;
+                    Data.currnetArticle = article;
                     for (var element in article) {
                         if (element == "title")
                             $("#articleTitle").html(article[element]);
+                        else if (element == "id")
+                            $("#articleId").html(article[element]);
                         else
-                            articles += "<p>" + element.toString() + ": " + article[element] + "</p>";
+                            articles += "<p id='article" + element.toString() + "'>" + element.toString() + ": " + article[element] + "</p>";
                     }
                     $("#article").html(articles);
-
-                    if (article) {
-                        $("#addFav").bind("click", function () {
-                            Article.checkLike($("#articleTitle").html());
-                        });
-                    }
-                    else {
+                    if (!article) {
                         $("#article").append("can't find this article.");
                     }
                 }
+
                 break;
+            case "favorite":
+                $("#favList").html("<p id='noEntries'>no entries in the list</p>");
 
-                //list all the subjects
-            case "subjects":
-                var txn = Data.db.transaction(["subjects"], "readonly");
-                var cursorRequest = txn.objectStore("subjects").openCursor();
-                var subjects = "";
-                var counter = 0;
-                cursorRequest.onsuccess = function (e) {
+                var txn = Data.db.transaction(["likes"], "readonly");
+                var statusStore = txn.objectStore("likes");
+                var request = statusStore.openCursor();
+                request.onsuccess = function (e) {
+                    var like = e.target.result;
+                    if (like) {
+                        var txn = Data.db.transaction(["articles"], "readonly");
+                        var store = txn.objectStore("articles");
+                        var request = store.get(parseInt(like.value["articleid"]));
+                        var likeid = like.value.id;
+                        request.onsuccess = function (e) {
+                            var article = e.target.result;
+                            if (article) {
+                                $("#favList").append("<p>" + article["title"] + " | <a class='article' id='article" + article["id"] + "' href='/pages/article/article.html'>show</a> | <a class='delete' id='delete" + likeid + "' href='#'>delete</a></p>");
+                                $(".article").unbind();
+                                $(".article").bind("click", function () {
+                                    Data.articleid = parseInt(this.id.slice(7, this.id.length));
+                                    WinJS.Navigation.navigate("/pages/article/article.html");
+                                });
 
-                    var cursor = e.target.result;
-                    if (cursor) {
-                        subjects += "<li id='subject" + cursor.value.id + "' class='subject'>" + cursor.value.name + "</li>";
-                        cursor.continue();
+                                $(".delete").unbind();
+                                $(".delete").bind("click", function () {
+                                    var record = this.id.slice(6, this.id.length);
+
+                                    var txn = Data.db.transaction(["likes"], "readwrite");
+                                    var statusStore = txn.objectStore("likes");
+                                    statusStore.delete(parseInt(record));
+
+                                    showData("favorite");
+                                });
+
+                                $("#noEntries").remove();
+                            }
+                        }
+                        like.continue();
                     }
-                    $("#subject").html("<li id='subject0' class='subject'>all</li>" + subjects);
-
-                    $(".subject").unbind();
-                    $(".subject").bind("click", function () {
-                        var subjectid = this.id.slice(7, this.id.length);
-                        Home.selectData(subjectid);
-                    });
-                }
-                break;
-
-            default:
+                };
                 break;
         }
     }
 
-    function updateLanguage() {
+    function initLanguage() {
         //get current language
         var applicationLanguages = Windows.Globalization.ApplicationLanguages.languages;
         if (!Data.language) {
@@ -282,20 +294,71 @@
         context.languages = languagesVector;
 
         //chang UI language
-        var changeContentArray = ["homeTitle", "homeFavlist", "articleFavlist", "favTitle"];
+        var changeContentArray = ["homeTitle", "homeFavlist", "addFav", "delFav", "favTitle"];
         for (var i in changeContentArray) {
             var element = changeContentArray[i];
             if (document.getElementById(element)) {
-                document.getElementById(element).textContent = resourceMap.getValue(element, context).valueAsString;
+                if (element == "addFav" || element == "delFav")
+                    document.getElementById(element).winControl._labelSpan.innerText = resourceMap.getValue(element, context).valueAsString;
+                else
+                    document.getElementById(element).textContent = resourceMap.getValue(element, context).valueAsString;
             }
         }
+    }
+    function updateLanguage() {
+        initLanguage();
 
+        //load new language data to db
+        var dbRequest = window.indexedDB.open("ArticleDB", 1);
+        dbRequest.onsuccess = function (evt) {
+            //clear objectstores of article and subject
+            Data.db = evt.target.result;
+
+            var txn = Data.db.transaction(["articles"], "readwrite");
+            var store = txn.objectStore("articles");
+            store.clear();
+
+            txn = Data.db.transaction(["subjects"], "readwrite");
+            store = txn.objectStore("subjects");
+            store.clear();
+
+            loadData(evt);
+        };
     }
 
     var groupedItems = list.createGrouped(
                     function groupKeySelector(item) { return item.group.key; },
                     function groupDataSelector(item) { return item.group; }
                 );
+
+    var favlist = new WinJS.Binding.List();
+
+    function favlistLoad() {
+        var length = Data.favlist.length;
+        for (var i = 0; i < length; i++) {
+            Data.favlist.pop();
+        }
+
+        var txn = Data.db.transaction(["likes"], "readonly");
+        var statusStore = txn.objectStore("likes");
+        var request = statusStore.openCursor();
+        request.onsuccess = function (e) {
+            var like = e.target.result;
+            if (like) {
+                var txn = Data.db.transaction(["articles"], "readonly");
+                var store = txn.objectStore("articles");
+                var request = store.get(parseInt(like.value["articleid"]));
+                var likeid = like.value.id;
+                request.onsuccess = function (e) {
+                    var article = e.target.result;
+                    if (article) {
+                        Data.favlist.push(article);
+                    }
+                }
+                like.continue();
+            }
+        };
+    }
 
     var db, articleid, favAddMsg, language = null;
 
@@ -304,14 +367,21 @@
         groups: groupedItems.groups,
         createDB: createDB,
         showData: showData,
+
+        initLanguage: initLanguage,
         updateLanguage: updateLanguage,
+
+        regionChange: loadArray,
 
         db: db,
         articleid: articleid,
         favAddMsg: favAddMsg,
         language: language,
 
-        myGroupedList: myGroupedList
+        myGroupedList: myGroupedList,
+
+        favlistLoad: favlistLoad,
+        favlist: favlist
     });
 
 })();
--- 888/pages/home/home.js
+++ 888/pages/home/home.js
@@ -1,57 +1,4 @@
-﻿var MyJSItemTemplate = WinJS.Utilities.markSupportedForProcessing(function MyJSItemTemplate(itemPromise) {
-    return itemPromise.then(function (currentItem) {
-
-        // Build ListView Item Container div
-        var result = document.createElement("div");
-        result.className = "regularListIconTextItem";
-        result.style.overflow = "hidden";
-
-        // Build content body
-        var body = document.createElement("div");
-        body.className = "regularListIconTextItem-Detail";
-        //body.style.overflow = "hidden";
-
-        // Display title
-        var title = document.createElement("h4");
-        title.innerText = currentItem.data.title;
-        body.appendChild(title);
-
-        // Display text
-        var fulltext = document.createElement("h6");
-        fulltext.innerText = currentItem.data.date;
-        body.appendChild(fulltext);
-
-        //put the body into the ListView Item
-        result.appendChild(body);
-        return result;
-    });
-});
-var headerTemplate = WinJS.Utilities.markSupportedForProcessing(function MyJSItemTemplate(itemPromise) {
-    return itemPromise.then(function (currentItem) {
-
-        // Build ListView Item Container div
-        var result = document.createElement("div");
-        result.className = "regularListIconTextItem";
-        result.style.overflow = "hidden";
-
-        // Build content body
-        var body = document.createElement("div");
-        body.className = "regularListIconTextItem-Detail";
-        body.style.overflow = "hidden";
-
-        // Display title
-        var title = document.createElement("h4");
-        title.innerText = currentItem.data.title;
-        body.appendChild(title);
-
-        //put the body into the ListView Item
-        result.appendChild(body);
-
-        return result;
-    });
-});
-
-(function () {
+﻿(function () {
     "use strict";
 
     WinJS.UI.Pages.define("/pages/home/home.html", {
@@ -59,60 +6,32 @@ var headerTemplate = WinJS.Utilities.markSupportedForProcessing(function MyJSIte
             // TODO: 在此初始化頁面。
             WinJS.Binding.processAll(element, this._data);
 
-            Data.updateLanguage();
+            Data.initLanguage();
             Data.createDB();
 
             isAddMsg();
-            $("#fav").click(function () {
-                WinJS.Navigation.navigate("/pages/favorite/favorite.html");
-            });
 
             var listView = element.querySelector("#listView").winControl;
             listView.addEventListener("iteminvoked", itemInvokedHandler);
 
+            $(".region").bind("click", function () {
+                var region = this.id.slice(6, this.id.length);
 
+                $(this).siblings().removeClass("region_c");
+                $(this).addClass("region_c");
+
+                Data.regionChange(region);
+            });
+
+            $("#navtoFav").bind("click", function () { navigatetoFav(); });
         }
     });
 
-    function selectData(subject) {
-        $("#output").html("subject: " + subject);
-        $("#output").append("<p id='noEntry'>no articles in subject " + subject + "</p>");
-
-        if (subject == 0)
-            Data.showData("articles");
-        else {
-            var txn = Data.db.transaction(["articles"], "readonly");
-            var cursorRequest = txn.objectStore("articles").openCursor();
-            cursorRequest.onsuccess = function (e) {
-                var cursor = e.target.result;
-                if (cursor) {
-                    if (cursor.value["subjectid"] == subject) {
-                        var str = "";
-                        for (var i in cursor.value) {
-                            if (i != "content")
-                                str += i.toString() + ": " + cursor.value[i] + " / ";
-                        }
-                        $("#noEntry").remove();
-                        $("#output").append("<p>" + str + "<a class='article' id='article" + cursor.value["id"] + "' href='/pages/article/article.html'>show</a></p>");
-
-                        $(".article").unbind();
-                        $(".article").bind("click", function () { showArticle(this); return false; });
-                    }
-                    cursor.continue();
-                }
-            }
-        }
-    }
-
     //navigate to article page
-    function showArticle(article) {
-        Data.articleid = article.id.slice(7, article.id.length);
-        WinJS.Navigation.navigate(article.href);
-    }
-
     function itemInvokedHandler(eventObject) {
         eventObject.detail.itemPromise.done(function (invokedItem) {
-            var itemData = invokedItem.data;
+            Data.articleid = invokedItem.data.id;
+            WinJS.Navigation.navigate("/pages/article/article.html");
         });
     }
 
@@ -123,8 +42,10 @@ var headerTemplate = WinJS.Utilities.markSupportedForProcessing(function MyJSIte
         }
     }
 
-    WinJS.Namespace.define("Home", {
-        selectData: selectData,
-        showArticle: showArticle
-    });
+    function navigatetoFav() {
+        document.getElementById('customLayoutAppBar').winControl.hide();
+        WinJS.Navigation.navigate('/pages/favorite/favorite.html');
+    }
+
+
 })();
